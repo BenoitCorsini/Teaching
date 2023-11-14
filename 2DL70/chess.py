5741708b49d19ea1226e4d8ff4742ef34ae74680
BenoitCorsini <benoitcorsini@gmail.com>
2023-11-14 21:20:13 +0100

    Chess running

--- 2DL70/chess.py
+++ 2DL70/chess.py
@@ -5,7 +5,6 @@ import os.path as osp
 import numpy as np
 import numpy.random as npr
 import matplotlib.patches as patches
-from matplotlib.path import Path
 from matplotlib.transforms import Affine2D
 from matplotlib.textpath import TextPath
 
@@ -19,6 +18,8 @@ CMAP = plot.get_cmap([
     (  0/255,   0/255,   0/255),
 ])
 PARAMS = {
+    'xmax' : N_TILES,
+    'ymax' : N_TILES,
     'colour_ratio' : 0.8,
     'inner_ratio' : 0.7,
     'disappear_ratio' : 0.3,
@@ -52,14 +53,21 @@ PARAMS = {
 }
 
 
-# https://www.rapidtables.com/code/text/unicode-characters.html
 class ChessPiece(object):
 
     def __init__(self, char):
         self.char = char
+        self.name = self.__class__.__name__.lower()
         self.counting = {}
         self.pos = 0, 0
         self.counting[self.pos] = 1
+        self.n_steps = 0
+
+    def reset(self):
+        self.counting = {}
+        self.pos = 0, 0
+        self.counting[self.pos] = 1
+        self.n_steps = 0
 
     @staticmethod
     def clean(neighbours):
@@ -72,23 +80,20 @@ class ChessPiece(object):
             self.counting[step] = 0
         self.counting[step] += 1
         self.pos = step
+        self.n_steps += 1
 
     def steps(self, jumps):
         for _ in range(jumps):
             self.step()
 
-    @classmethod
-    def get_path(piece, i=0, j=0, inner_ratio=1):
-        path = TextPath((0, 0), piece().char)
-        bbox = path.get_extents()
-        size = max(bbox.size)
-        transform = Affine2D()
-        tx, ty =  (size - bbox.size)/2 - bbox.p0
-        transform.translate(tx, ty)
-        transform.scale(2*inner_ratio/size/N_TILES)
-        transform.translate(2*i/N_TILES - 1 + (1 - inner_ratio)/N_TILES, 2*j/N_TILES - 1 + (1 - inner_ratio)/N_TILES)
-        path = path.transformed(transform)
-        return path
+    def get_path(self, inner_ratio=1, i=0, j=0):
+        return plot.path_from_string(
+            self.char,
+            ratio=inner_ratio,
+            x=i + 0.5,
+            y=j + 0.5,
+        )
+
 
 class King(ChessPiece):
 
@@ -167,18 +172,11 @@ class Knight(ChessPiece):
             (i - 1, j - 2),
         ])
 
+
 class ChessPlot(plot):
 
-    def __init__(self, steps=1, jumps=1, **kwargs):
+    def __init__(self):
         super().__init__(**PARAMS)
-        self.steps = steps
-        self.jumps = jumps
-
-    def tile_to_xy(self, i, j, simple_dim=False):
-        if simple_dim:
-            return np.array([2*i/N_TILES - 1, 2*j/N_TILES - 1])
-        else:
-            return np.array([[2*i/N_TILES - 1, 2*j/N_TILES - 1]])
 
     def setup_chess(self, piece):
         self.tiles = {}
@@ -188,9 +186,9 @@ class ChessPlot(plot):
             for j in range(N_TILES):
                 self.tiles[i, j] = self.plot_shape(
                     shape_name='Rectangle',
-                    xy=self.tile_to_xy(i, j, True),
-                    height=2/N_TILES,
-                    width=2/N_TILES,
+                    xy=[i, j],
+                    height=1,
+                    width=1,
                     **self.tile_params
                 )
                 path = piece.get_path(i=i, j=j, inner_ratio=self.inner_ratio)
@@ -204,7 +202,6 @@ class ChessPlot(plot):
                     path=path,
                     **self.bg_piece_params
                 )
-
         self.grid(N_TILES, **self.grid_params)
         self.update_tiles()
 
@@ -226,93 +223,69 @@ class ChessPlot(plot):
             counts = 0.5*np.ones_like(counts)
         for (i, j), tile in self.tiles.items():
             self.tiles[i, j].set_color(self.cmap(ratio*((i + j) % 2) + counts[i, j]*(1 - ratio)))
-            # self.tiles[i, j].set_color(self.cmap(ratio*(1 - counts[i,j])*((i + j) % 2) + counts[i, j]))
             self.pieces[i, j].set_visible(False)
             self.bg_pieces[i, j].set_visible(False)
 
-    def image(self, piece, *args, **kwargs):
+    def file_name(self, piece, secret=False):
+        name = f'chess_{piece.name}_{piece.n_steps}'
+        if secret:
+            name += '_secret'
+        return name
+
+    def image(self, piece, secret=False):
         self.reset()
         self.setup_chess(piece)
         self.update_tiles(piece.counting, 0)
-        self.pieces[0, 0].set_visible(True)
-        self.bg_pieces[0, 0].set_visible(True)
+        self.pieces[0, 0].set_visible(not secret)
+        self.bg_pieces[0, 0].set_visible(not secret)
+        self.save_image(name=self.file_name(piece, secret))
 
-        self.save(name='piece_' + piece.__class__.__name__.lower())
-        print('Time to plot image: ' + self.time())
-
-    def animate(self, piece, *args, **kwargs):
+    def video(self, piece, steps=0, jumps=1, secret=False):
         self.reset()
         self.setup_chess(piece)
         self.update_tiles(piece.counting, 1)
-        self.pieces[0, 0].set_visible(True)
-        self.bg_pieces[0, 0].set_visible(True)
+        self.pieces[piece.pos].set_visible(not secret)
+        self.bg_pieces[piece.pos].set_visible(not secret)
         for _ in range(int(self.fps*self.times['initial'])):
             self.new_frame()
         n_steps = 1
-        for step in range(self.steps):
-            n_steps *= self.jumps
-            ratio = (self.disappear_ratio - step/self.steps)/self.disappear_ratio
+        for step in range(steps):
+            n_steps *= jumps
+            ratio = (self.disappear_ratio - step/steps)/self.disappear_ratio
             if ratio < 0:
                 ratio = 0
             self.update_tiles(piece.counting, ratio)
-            self.pieces[piece.pos].set_visible(True)
-            self.bg_pieces[piece.pos].set_visible(True)
+            self.pieces[piece.pos].set_visible(not secret)
+            self.bg_pieces[piece.pos].set_visible(not secret)
             self.new_frame()
             piece.steps(int(n_steps))
         self.update_tiles(piece.counting, 0)
-        self.pieces[0, 0].set_visible(True)
-        self.bg_pieces[0, 0].set_visible(True)
+        self.pieces[0, 0].set_visible(not secret)
+        self.bg_pieces[0, 0].set_visible(not secret)
         for _ in range(int(self.fps*self.times['final'])):
             self.new_frame()
+        self.save_video(name=self.file_name(piece, secret))
 
-        sys.stdout.write('\033[F\033[K')
-        print(f'Time to create all frames ({self.frame_counter}): ' + self.time())
-        print('Making the video...')
-        self.frames_to_video(name='piece_' + piece.__class__.__name__.lower())
-        sys.stdout.write('\033[F\033[K')
-        print('Time to make video: ' + self.time())
-
-    def anonymized(self, piece):
-        self.reset()
-        self.setup_chess(piece)
-        piece.steps(self.steps*self.jumps)
-        self.update_tiles(piece.counting, 0)
-        self.save(name='piece_' + piece.__class__.__name__.lower() + f'_{self.steps}_anonymized')
-        self.pieces[0, 0].set_visible(True)
-        self.bg_pieces[0, 0].set_visible(True)
-
-        self.save(name='piece_' + piece.__class__.__name__.lower() + f'_{self.steps}')
-        print('Time to plot image: ' + self.time())
-
-    def moves(self, piece):
-        self.reset()
-        self.setup_chess(piece)
-        i, j = 3, 3
-        counting = {n : 1 for n in piece.neighbours(i, j)}
-        self.update_tiles(counting, 0.5)
-        self.pieces[i, j].set_visible(True)
-        self.bg_pieces[i, j].set_visible(True)
-        self.save(name='moves_' + piece.__class__.__name__.lower())
-        self.update_tiles({}, 0.5)
-        self.pieces[i, j].set_visible(True)
-        self.bg_pieces[i, j].set_visible(True)
-        self.save(name='no_moves_' + piece.__class__.__name__.lower())
-        print('Time to plot image: ' + self.time())
-
-    def run(self, piece):
-        # self.animate(piece)
-        self.image(piece)
-        # self.anonymized(piece)
-        # self.moves(piece)
+    def run(self, piece, steps=0, jumps=0, secret=False, seed=None):
+        npr.seed(seed)
+        n_steps = 1
+        for step in range(steps):
+            n_steps *= jumps
+            piece.steps(int(n_steps))
+        self.image(piece, secret=secret)
+        piece.reset()
+        npr.seed(seed)
+        self.video(piece, steps=steps, jumps=jumps, secret=secret)
+        piece.reset()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=None)
     parser.add_argument('--steps', type=int, default=180)
     parser.add_argument('--jumps', type=int, default=1.05)
+    parser.add_argument('--secret', type=int, default=0)
     kwargs = vars(parser.parse_args())
-    ChessPlot(**kwargs).run(King())
-    ChessPlot(**kwargs).run(Queen())
-    ChessPlot(**kwargs).run(Rook())
-    ChessPlot(**kwargs).run(Bishop())
-    ChessPlot(**kwargs).run(Knight())
+    CP = ChessPlot()
+    for piece in [King(), Queen(), Rook(), Bishop(), Knight()]:
+        CP.run(piece, **kwargs)
