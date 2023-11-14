2c447697e9d5f6f2164678836cb605f6f372abe7
BenoitCorsini <benoitcorsini@gmail.com>
2023-11-14 19:36:15 +0100

    New grid function

--- bcplot/image.py
+++ bcplot/image.py
@@ -13,23 +13,36 @@ class Image(Figure):
         self.ax.add_patch(patch)
         return patch
 
-    def grid(self, n_blocks=10, **kwargs):
+    def grid(self, x_blocks=1, y_blocks=None, xmin=None, xmax=None, ymin=None, ymax=None, **kwargs):
+        if y_blocks is None:
+            y_blocks = x_blocks
+        if xmin is None:
+            xmin = self.xmin
+        if xmax is None:
+            xmax = self.xmax
+        if ymin is None:
+            ymin = self.ymin
+        if ymax is None:
+            ymax = self.ymax
         columns = []
         rows = []
         kwargs['capstyle'] = 'butt'
-        delta = 2/n_blocks
-        for i in range(0, n_blocks+1):
-            pos = i*delta - 1
-            column_patch = patches.Polygon(
-                [[pos,-1], [pos,1]],
+        delta_x = (xmax - xmin)/x_blocks
+        for x in range(0, x_blocks+1):
+            pos = xmin + x*delta_x
+            column_patch = self.plot_shape(
+                shape_name='Polygon',
+                xy=[[pos, xmin], [pos, xmax]],
                 **kwargs
             )
-            self.ax.add_patch(column_patch)
             columns.append(column_patch)
-            row_patch = patches.Polygon(
-                [[-1,pos], [1,pos]],
+        delta_y = (ymax - ymin)/y_blocks
+        for y in range(0, y_blocks+1):
+            pos = ymin + y*delta_y
+            row_patch = self.plot_shape(
+                shape_name='Polygon',
+                xy=[[xmin, pos], [xmax, pos]],
                 **kwargs
             )
-            self.ax.add_patch(row_patch)
             rows.append(row_patch)
         return columns, rows
