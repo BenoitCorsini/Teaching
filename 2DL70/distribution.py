e3268754bb04d7fabca83cc495d061bb26632ea3
BenoitCorsini <benoitcorsini@gmail.com>
2023-11-16 20:04:07 +0100

    First distribution visual

--- 2DL70/distribution.py
+++ 2DL70/distribution.py
@@ -12,6 +12,58 @@ CMAP = plot.get_cmap([
     (  0/255,   0/255,   0/255),
 ])
 PARAMS = {
+    'extra_left' : 0.1,
+    'extra_right' : 0.1,
+    'extra_bottom' : 0.1,
+    'extra_top' : 0.1,
+    'max_ticks' : 15,
+    'tick_height' : 0.01,
+    'label_xshift' : 0.1,
+    'label_yshift' : 0.01,
+    'label_height' : 0.02,
+    'grid_params' : {
+        'lw' : 1,
+        'color' : CMAP(0.75),
+        'zorder' : 1,
+        'alpha' : 0.1,
+    },
+    'axis_params' : {
+        'lw' : 2,
+        'color' : CMAP(0.75),
+        'zorder' : 2,
+        'capstyle' : 'round',
+    },
+    'label_params' : {
+        'lw' : 1,
+        'color' : CMAP(0.75),
+        'zorder' : 2,
+        'capstyle' : 'round',
+        'joinstyle' : 'round',
+    },
+    'pmf_bar_params' : {
+        'lw' : 10,
+        'color' : CMAP(0.6),
+        'zorder' : 0,
+    },
+    'pmf_marker_params' : {
+        'lw' : 0,
+        'color' : CMAP(0.6),
+        'marker' : 'o',
+        'ms' : 18,
+        'zorder' : 0,
+    },
+    'cdf_bar_params' : {
+        'lw' : 10,
+        'color' : CMAP(0.4),
+        'zorder' : 0,
+    },
+    'cdf_marker_params' : {
+        'lw' : 0,
+        'color' : CMAP(0.4),
+        'marker' : 'o',
+        'ms' : 18,
+        'zorder' : 0,
+    },
 }
 
 
@@ -27,7 +79,6 @@ class Distribution(object):
         return np.sqrt(self.variance)
 
     def PMF(self, x):
-        assert self.is_discrete
         if isinstance(x, list):
             x = np.array(x)
         if isinstance(x, np.ndarray):
@@ -121,19 +172,149 @@ class DistributionPlot(plot):
         super().__init__(**PARAMS)
 
     def file_name(self):
-        return 'discrete'
+        return 'distribution'
 
-    def image(self, distribution):
+    def get_ticks(self, bounds, extras, axis, step=1):
+        if extras is None:
+            extras = self.extra_bottom, self.extra_top
+        minimum = (1 + extras[0])*bounds[0] - extras[0]*bounds[1]
+        maximum = (1 + extras[1])*bounds[1] - extras[1]*bounds[0]
+        step *= np.ceil((maximum - minimum)/step/self.max_ticks)
+        ticks = np.concatenate([
+            - np.arange(step, - int(minimum) + 1 + step, step=step)[::-1],
+            np.arange(0, int(maximum) + 1 + step, step=step),
+        ])
+        ticks = ticks[(minimum - step <= ticks) & (ticks <= maximum + step)]
+        setattr(self, axis + 'min', minimum)
+        setattr(self, axis + 'max', maximum)
+        return ticks
+
+    def plot_ticks(self, xticks, yticks):
+        self.plot_shape(
+            shape_name='Rectangle',
+            xy=(self.xmin, 0),
+            width=self.xmax - self.xmin,
+            height=0,
+            **self.axis_params
+        )
+        self.plot_shape(
+            shape_name='Rectangle',
+            xy=(0, self.ymin),
+            width=0,
+            height=self.ymax - self.ymin,
+            **self.axis_params
+        )
+        label = self.path_from_string(
+            s=str(0),
+            x= - self.label_xshift,
+            y= - self.label_yshift,
+            height=self.label_height,
+            anchor='north east',
+        )
+        self.plot_shape(
+            shape_name='PathPatch',
+            path=label,
+            **self.label_params
+        )
+        for xt in xticks:
+            if xt:
+                self.plot_shape(
+                    shape_name='Rectangle',
+                    xy=(xt, - self.tick_height/2),
+                    width=0,
+                    height=self.tick_height,
+                    **self.axis_params
+                )
+                label = self.path_from_string(
+                    s=str(xt),
+                    x=xt - self.label_xshift,
+                    y= - self.label_yshift,
+                    height=self.label_height,
+                    anchor='north east',
+                )
+                self.plot_shape(
+                    shape_name='PathPatch',
+                    path=label,
+                    **self.label_params
+                )
+        tick_width = self.tick_height*self.x_over_y
+        for yt in yticks:
+            if yt:
+                self.plot_shape(
+                    shape_name='Rectangle',
+                    xy=(- tick_width/2, yt),
+                    width=tick_width,
+                    height=0,
+                    **self.axis_params
+                )
+                label = self.path_from_string(
+                    s=str(yt),
+                    x= - self.label_xshift,
+                    y=yt - self.label_yshift,
+                    height=self.label_height,
+                    anchor='north east',
+                )
+                self.plot_shape(
+                    shape_name='PathPatch',
+                    path=label,
+                    **self.label_params
+                )
+
+    def plot_axis(self, xticks, yticks):
+        self.grid(
+            x_blocks=np.size(xticks) - 1,
+            y_blocks=np.size(yticks) - 1,
+            xmin=np.min(xticks),
+            xmax=np.max(xticks),
+            ymin=np.min(yticks),
+            ymax=np.max(yticks),
+            **self.grid_params
+        )
+        self.plot_ticks(xticks, yticks)
+
+    def plot_pmf(self, distribution):
+        x = distribution.range(max(self.xmax, - self.xmin))
+        y = distribution.PMF(x)
+        self.ax.plot(x, y, **self.pmf_marker_params)
+        points = list(zip(x, y))
+        for x, y in points:
+            self.plot_shape(
+                shape_name='Rectangle',
+                xy=(x, 0),
+                width=0,
+                height=y,
+                **self.pmf_bar_params
+            )
+
+    def plot(self, distribution, bound):
+        bounds = distribution.range(bound)
+        bounds = np.min(bounds), np.max(bounds)
+        xticks = self.get_ticks(
+            bounds=bounds,
+            extras=(self.extra_left, self.extra_right),
+            axis='x',
+        ).astype(int)
+        yticks = self.get_ticks(
+            bounds=(0, 1),
+            extras=(self.extra_bottom, self.extra_top),
+            axis='y',
+            step=0.25,
+        )
+        self.x_over_y = (self.xmax - self.xmin)/(self.ymax - self.ymin)*self.figsize[1]/self.figsize[0]
         self.reset()
+        self.plot_axis(xticks, yticks)
+        self.plot_pmf(distribution)
+
+    def image(self, distribution, bound):
+        self.plot(distribution, bound)
         self.save_image(name=self.file_name())
 
     def video(self):
         self.reset()
         self.save_video(name=self.file_name())
 
-    def run(self, distribution, seed=None):
-        npr.seed(seed)
-        self.image(distribution)
+    def run(self, distribution, bound=None):
+        self.image(distribution, bound)
         # self.video()
 
 
